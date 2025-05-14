1e70735a9e5225ab35a9c7656240af09e925db4e
keyur1411 <keyur14113@gmail.com>
2025-05-14 17:52:59 +0530

    backend server url

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 import '../styles/Home.css';
 
 // Socket connection
-const socket = io('http://localhost:5000');
+const socket = io('process.env.REACT_APP_SERVER_URL');
 
 function Home() {
   const navigate = useNavigate();
